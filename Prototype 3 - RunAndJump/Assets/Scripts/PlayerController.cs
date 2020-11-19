5749eb7178c80814c865555206aeda0d4df051a9
Mikhail <sergeevmisha16@gmail.com>
2020-11-19 20:30:43 +0300

    init

---
+++ Prototype 3 - RunAndJump/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool gameOver;
+    public bool isOnGround = true;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private Rigidbody rb;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravityModifier;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1.0f); 
+            dirtParticle.Stop();
+        }
+            
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+            dirtParticle.Play();
+            isOnGround = true;
+        }
+        
+    }
+}
